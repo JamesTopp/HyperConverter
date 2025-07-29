8195de90dedff16643511705e96538497520ff13
JamesTopp <itsjamestopp@gmail.com>
2025-07-29 14:10:48 -0700

    update triangle colour

--- content.js
+++ content.js
@@ -264,7 +264,7 @@ style.textContent = `
     left: 0px !important;
     width: 6px !important;
     height: 6px !important;
-    background-color: #FFC8A2 !important;
+    background-color: #9B7EBF !important;
     pointer-events: none !important;
     z-index: 1 !important;
     clip-path: polygon(0% 0%, 0% 100%, 100% 100%) !important;
