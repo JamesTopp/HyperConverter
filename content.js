858e0a9e1f5629af7806e5b6e0e5d20365e6d21f
JamesTopp <itsjamestopp@gmail.com>
2025-09-16 15:46:16 -0700

    again

--- content.js
+++ content.js
@@ -701,7 +701,7 @@ style.textContent = `
     left: 0px !important;
     width: 6px !important;
     height: 6px !important;
-    background-color: #B8A9C9 !important;
+    background-color: #C8B5DB !important;
     pointer-events: none !important;
     z-index: 1 !important;
     clip-path: polygon(0% 0%, 0% 100%, 100% 100%) !important;
